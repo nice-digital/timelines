b9675edf7e104deafc2f8e1284067d4d636b8703
Sue Garner <Sue.Garner@nice.org.uk>
2021-06-25 08:15:52 +0100

    TTM-27 Create empty project

---
+++ NICE.Timelines/NICE.Timelines/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+
+namespace NICE.Timelines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //unusually for a console app, using appsettings.json + secrets.json for configuration, for consistency with other projects and also for the secrets support - because it's a public repo.
+            IConfiguration Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddUserSecrets(Assembly.GetExecutingAssembly(), optional: true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
+        }
+    }
+}
