6825d79c9ed313efe2e5a29ee9088785f188fcb1
Sue Garner <Sue.Garner@nice.org.uk>
2021-08-18 09:19:32 +0100

    TTM-27 Set to use RabbitMQ

--- NICE.Timelines/NICE.Timelines.ConsoleApp/Services/SyncService.cs
+++ NICE.Timelines/NICE.Timelines.ConsoleApp/Services/SyncService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using NICE.Timelines.Configuration;
 
 namespace NICE.Timelines.Services
@@ -13,27 +14,31 @@ namespace NICE.Timelines.Services
     {
         private readonly IClickUpService _clickUpService;
         private readonly ClickUpConfig _clickUpConfig;
+        private readonly ILogger<ISyncService> _logger;
 
-        public SyncService(ClickUpConfig clickUpConfig, IClickUpService clickUpService)
+        public SyncService(ClickUpConfig clickUpConfig, IClickUpService clickUpService, ILogger<ISyncService> logger)
         {
             _clickUpConfig = clickUpConfig;
             _clickUpService = clickUpService;
+            _logger = logger;
         }
 
         public async Task Process()
         {
             Console.WriteLine("Started processing");
+            _logger.LogInformation("Started processing");
 
             foreach (var spaceId in _clickUpConfig.SpaceIds)
             {
                 Console.WriteLine($"Started with space: {spaceId}");
-
+                
                 var record = await _clickUpService.ProcessSpace(spaceId);
 
                 Console.WriteLine($"finished with space: {spaceId} records saved or updated: {record}");
             }
 
             Console.WriteLine("Ended processing");
+            _logger.LogInformation("Ended processing");
         }
     }
 }
